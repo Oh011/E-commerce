2086bb30846e7f31290b86e25d8901f2d64264b2
omar <oh1149725@gmail.com>
2024-09-02 11:42:54 +0300

    new

---
+++ chunk-D4UWMXR6.js
@@ -0,0 +1,1 @@
+import{ca as n}from"./chunk-TGYZD3EX.js";var o=(()=>{let t=class t{transform(i,e){return i.split(" ",e).join(" ")}};t.\u0275fac=function(e){return new(e||t)},t.\u0275pipe=n({name:"tremtext",type:t,pure:!0,standalone:!0});let r=t;return r})();export{o as a};
